e68acc3b425d1d073cee5f0b2977eb20eb663b23
yxl-net <996986842@qq.com>
2021-09-23 20:50:10 +0800

    项目版本初始化

---
+++ Yxl/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace Yxl
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.ts = new System.Windows.Forms.ToolStrip();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ts
+            // 
+            this.ts.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.ts.Location = new System.Drawing.Point(0, 0);
+            this.ts.Name = "ts";
+            this.ts.Size = new System.Drawing.Size(800, 31);
+            this.ts.TabIndex = 0;
+            this.ts.Text = "toolStrip1";
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToOrderColumns = true;
+            this.dgv.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.dgv.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgv.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCellsExceptHeader;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("SimSun", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgv.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column10,
+            this.Column9});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 31);
+            this.dgv.MultiSelect = false;
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 51;
+            this.dgv.RowTemplate.Height = 27;
+            this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgv.Size = new System.Drawing.Size(800, 419);
+            this.dgv.TabIndex = 1;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "title";
+            this.Column2.Frozen = true;
+            this.Column2.HeaderText = "标题";
+            this.Column2.MinimumWidth = 36;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 36;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "version";
+            this.Column3.HeaderText = "版本";
+            this.Column3.MinimumWidth = 36;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 36;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "authors";
+            this.Column4.HeaderText = "作者";
+            this.Column4.MinimumWidth = 36;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 36;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "license";
+            this.Column5.HeaderText = "协议";
+            this.Column5.MinimumWidth = 36;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 36;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "projectUrl";
+            this.Column6.HeaderText = "网址";
+            this.Column6.MinimumWidth = 36;
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 36;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "description";
+            this.Column7.HeaderText = "描述";
+            this.Column7.MinimumWidth = 36;
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 36;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "tags";
+            this.Column8.HeaderText = "标签";
+            this.Column8.MinimumWidth = 36;
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            this.Column8.Width = 36;
+            // 
+            // Column10
+            // 
+            this.Column10.DataPropertyName = "dir";
+            this.Column10.HeaderText = "项目目录";
+            this.Column10.MinimumWidth = 72;
+            this.Column10.Name = "Column10";
+            this.Column10.ReadOnly = true;
+            this.Column10.Width = 72;
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "key";
+            this.Column9.HeaderText = "密钥";
+            this.Column9.MinimumWidth = 36;
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
+            this.Column9.Width = 36;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.ts);
+            this.Name = "Form1";
+            this.Text = "Nuget 发布";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip ts;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column10;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+    }
+}
+
---
+++ Yxl/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yxl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("title");
+            dt.Columns.Add("version");
+            dt.Columns.Add("authors");
+            dt.Columns.Add("license");
+            dt.Columns.Add("projectUrl");
+            dt.Columns.Add("description");
+            dt.Columns.Add("tags");
+            dt.Columns.Add("dir");
+            dt.Columns.Add("key");
+
+            dt.Rows.Add("Yxl.ORM","0.0.3", "yxl-net(996986842@qq.com)", "Apache-2.0", "https://github.com/yxl-net/orm", "最简单的SQL Server对象关系库", "SQL_Server,O/RM,最简单", @"G:\yxl\Yxl\Yxl.ORM", "oy2ajlmen5frpu5gfklacqdejigp6fjerckx7c6stlpkai");
+            /*
+             nuget.exe pack
+             nuget.exe push Yxl.ORM.0.0.2.nupkg oy2ajlmen5frpu5gfklacqdejigp6fjerckx7c6stlpkai -Source https://api.nuget.org/v3/index.json
+             */
+            dgv.DataSource = dt;
+        }
+    }
+}
